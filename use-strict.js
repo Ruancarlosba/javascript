ef18b6e38c7c1c182f9eea2e9ab2cef1ce681513
Ruancarlosba <144129403+Ruancarlosba@users.noreply.github.com>
2024-08-25 21:33:43 -0300

    atualizaçao

---
+++ use-strict.js
@@ -0,0 +1,25 @@
+//use strict deixa o js mais seguro
+
+
+/*y = 10
+
+function foo() {
+
+    "use strict"
+    
+    x = 20
+}
+
+foo()
+console.log(x)
+console.log(y)*/
+
+function dobrar(n1, n1) {
+
+    "use strict"
+    console.log(n1, n1)
+    return n1 * n1
+}
+
+console.log(dobrar(5,5))
+
