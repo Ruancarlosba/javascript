041285a9d5162d703ea2ec4de40587c04d23ed7c
Ruancarlosba <144129403+Ruancarlosba@users.noreply.github.com>
2024-08-29 18:56:48 -0300

    atualizaçao

--- funcoes/callback.js
+++ funcoes/callback.js
@@ -1,4 +1,16 @@
+//funçao de callback
 function teste(cb) {
-    console.log("funcao teste", (n + n))
+    console.log("funcao teste")
+    cb()
 }
-teste()
+
+// chamando com a funçao anonima
+/*teste(function(){
+    console.log("funçao anonima de callback")
+})*/
+
+// chamando com funçoes declaráveis
+function fn() {
+    console.log("funçao declarativa de callback")
+}
+teste(fn)
