f9041ef8ecdbc5fed32bed9a04793436aa652aed
Ruancarlosba <144129403+Ruancarlosba@users.noreply.github.com>
2024-09-04 17:48:36 -0300

    atualizaçao

--- dom_saudacao/js/saudacao.js
+++ dom_saudacao/js/saudacao.js
@@ -1,7 +1,18 @@
 (function(){
-    const nomeUsuario = "Daniel"
-    //document.querySelector(".top-bar p").textContent = "Bem-Vindo(a)," +nomeUsuario
+    const nomeUsuario = null
     const elemento = document.querySelector(".top-bar p")
-    //elemento.textContent += nomeUsuario
-    elemento.innerHTML += "<b>" + nomeUsuario + "</b>"
+    
+    
+    if(nomeUsuario) {
+
+        //document.querySelector(".top-bar p").textContent = "Bem-Vindo(a)," +nomeUsuario
+        
+        //elemento.textContent += nomeUsuario
+        elemento.innerHTML += "<b>" + nomeUsuario + "</b>"
+    } else {
+        //elemento.parentElement.style.display = "none"
+        const elementoParaRemover = elemento.parentElement
+        elementoParaRemover.parentElement.removeChild(elementoParaRemover)
+
+    }
 })()
