6e0276c0bf0c5e4f9e8ca8caaee47d399418efb4
Ruancarlosba <144129403+Ruancarlosba@users.noreply.github.com>
2024-09-18 19:24:17 -0300

    Atualizaçaõ

--- arrays/array01.js
+++ arrays/array01.js
@@ -1,8 +1,45 @@
 const arr = [1, 5, 10, "ola", true]
 
-let shNumeros = arr.some(function(el){
+
+
+/*some() - */
+
+/*let some_Numeros = arr.some(function(el){
    
     return typeof el === "number" && el > 20
 })
 
-console.log(shNumeros)
+console.log(some_Numeros)*/
+
+/*every() - */
+
+/*let every_Numeros = arr.every(function(el){
+
+    return typeof el === "number"
+})
+
+console.log(every_Numeros)*/
+
+/*filter() - */
+
+ let arr1 = arr.filter(function(elemento,indice,_arr){
+   
+    return typeof elemento === "number"
+})
+
+//console.log(arr)
+console.log(arr1)
+
+/*forEach - loop mais elegante */
+
+/*arr.forEach(function(el, i, _arr){
+   console.log(i, ":",el)
+})
+
+console.log(arr)*/
+
+arr1 = arr1.map(function(el, i, _arr){
+    return el * el
+})
+
+console.log(arr1)
