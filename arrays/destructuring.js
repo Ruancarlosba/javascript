968f8861a73ba9f7a00c2d3aaac1ba2b14c858cc
Ruancarlosba <144129403+Ruancarlosba@users.noreply.github.com>
2024-10-27 09:44:22 -0300

    atualizaçaõ

---
+++ arrays/destructuring.js
@@ -0,0 +1,16 @@
+const arr = [10,20,30,40,50,60]
+
+const [n1,n2] = arr
+console.log(n1)
+console.log(n2)
+
+console.log("----------------------------------------")
+
+const pessoa = {
+    nome: 'Joaõ',
+    idade: 20
+}
+
+let {nome,idade} = pessoa
+console.log(nome)
+console.log(idade)
--- arrays/treino.js
+++
@@ -1,8 +0,0 @@
-const arr = ["maça", "banana","banana", "Uva"]
-
-let multi = arr.reduce(function(acumulador, fruta, indiceAtual, ArrayOriginal){
-     acumulador[fruta] = (acumulador[fruta] || 0) + 1
-    return acumulador
-}, {})
-
-console.log(multi)
